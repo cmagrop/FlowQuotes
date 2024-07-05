47650feda757de3ab47ac3308cc9006c5c7c9f1f
=Christian Magro Pérez <cmagrop@gmail.com>
2024-07-04 23:06:42 -0400

    Modificacion MainActivity y MainViewModel

--- app/src/main/java/com/example/mvvmflowsquotes/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmflowsquotes/view/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.example.mvvmflowsquotes.R
 import com.example.mvvmflowsquotes.databinding.ActivityMainBinding
+import com.example.mvvmflowsquotes.util.shareText
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,35 +18,59 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel = MainViewModel(this)
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //llamar setupUI
-        //llamar listenToViewModel
+        //setupUI() //llama a la interfaz de usuario
+        //listenToViewModel() //escuchar los cambios en el ViewModel
 
     }
 
     private fun setupUI() = with(binding) {
-        //quoteBtn.setOnClickListener{viewModel.emitAction(QuoteButtonClick)}
-        //shareBtn.setOnClickListener{viewModel.emitAction(ShareButtonClick)}
+        quoteBtn.setOnClickListener{viewModel.emitAction(QuoteButtonClick)}
+        shareBtn.setOnClickListener{viewModel.emitAction(ShareButtonClick)}
 
     }
 
     private fun listenToViewModel()
     {
+        //creamos una  corrutina del ciclo de vida de un Activity
+        lifecycleScope.launch{
+            viewModel.state.flowWithLifecycle(lifecycle).collect{
+                render(it)
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.viewEvents.flowWithLifecycle(lifecycle).collect{
+                handleEvent(it)
+            }
+        }
 
     }
 
-    private fun render() //completar
+    // rendenderizar la vista
+    private fun render(state: MainViewState)  = with(binding)
     {
+        author.text = state.author
+        quote.text= state.quote
+        quoteBtn.isEnabled= state.isButtonEnabled
+        shareBtn.isEnabled = state.isButtonEnabled
+        spinner.visibility= state.spinnerVisibility.value
 
     }
 
-    private fun handleEvent()
+    private fun handleEvent(event: MainViewEvent)
     {
+        when(event)
+        {
+            is MainViewEvent.Share -> shareText(event.text)
+        }
 
     }
 
--- app/src/main/java/com/example/mvvmflowsquotes/view/MainViewModel.kt
+++ app/src/main/java/com/example/mvvmflowsquotes/view/MainViewModel.kt
@@ -1,5 +1,90 @@
 package com.example.mvvmflowsquotes.view
 
-class MainViewModel(mainActivity: MainActivity) {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvvmflowsquotes.interactor.GetQuoteInteractor
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
-}
+//conecta la Vista con el Modelo
+class MainViewModel
+    (context: Context,
+     private val interactor: GetQuoteInteractor = GetQuoteInteractor(context),
+     private val stateHandler: MainViewStateHandler = MainViewStateHandler()): ViewModel()
+    {
+         //atributos de MainViewModel
+         private val viewEventsChannel= Channel<MainViewEvent>(Channel.BUFFERED)
+         private val mutableState: MutableStateFlow<MainViewState> =
+             MutableStateFlow(stateHandler.handleInitialState()) //estamos estableciendo el estado inicial del ciclo
+
+        val state = mutableState.asStateFlow()  // establece estado cambiante como un flujo de estados
+        val viewEvents = viewEventsChannel.receiveAsFlow() //recibe los eventos como un flujo
+
+        init {
+            fetchNewQuote()
+
+        }
+
+
+
+        fun emitAction(action: MainViewAction)
+        {
+            when(action)
+            {
+                QuoteButtonClick -> fetchNewQuote()
+                ShareButtonClick -> shareQuote(state.value.author,state.value.quote)
+
+            }
+
+            /*
+            * switch(action)
+            * {
+            *  case QuoteButtonClick:
+            *  fectNewQuote
+            * case ShareButtonClick:
+            * shareQuote(state.value.author,state.value.quote)
+            *
+            * }
+            * */
+
+        }
+
+        private fun shareQuote(author: String, quote: String) {
+
+            emitViewEvent(
+                MainViewEvent.Share("$author: $quote")
+            )
+
+        }
+
+        private fun emitViewEvent(event: MainViewEvent) = viewModelScope.launch {
+
+            viewEventsChannel.send(event)
+
+        }
+
+
+        private fun fetchNewQuote() {
+
+            viewModelScope.launch {
+
+                emitState(stateHandler.handleLoadingState(state.value))
+                val quote = interactor.getQuote()
+                emitState(stateHandler.handleSuccess(quote))
+
+            }
+//
+        }
+
+        private fun emitState(state: MainViewState) {
+
+            mutableState.value = state
+
+        }
+
+
+    }
